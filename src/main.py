c66cb441dbfb834ac8913c399210acec48777079
Eric Johnson <micromashor@gmail.com>
2024-12-19 17:44:54 -0600

    allow setting status message

--- src/main.py
+++ src/main.py
@@ -25,7 +25,7 @@ def main() -> int | None:
             logger.error(f"Must provide {description}.")
             return 1
 
-    bot = W0eeeBot(os.environ['BOT_DB_URL'], os.environ['ULS_DB_URL'], version)
+    bot = W0eeeBot(os.environ['BOT_DB_URL'], os.environ['ULS_DB_URL'], os.environ.get("PRESENCE_LISTENINGTO"), version)
     bot.run(os.environ['DISCORD_BOT_TOKEN'], root_logger=True)
     
 if __name__ == '__main__':
--- src/w0eeebot.py
+++ src/w0eeebot.py
@@ -9,11 +9,12 @@ logger = logging.getLogger("W0EEEBot")
 
 class W0eeeBot(discord.Client):
     version: str
-    def __init__(self, bot_db_url: str, uls_db_url: str, version: str):        
+    def __init__(self, bot_db_url: str, uls_db_url: str, listening_to: str | None, version: str):        
         super().__init__(intents=discord.Intents.default())
         
         self.bot_db_url = bot_db_url
         self.uls_db_url = uls_db_url
+        self.listening_to = listening_to
         self.version = version
         
         root = app_commands.CommandTree(self)
@@ -31,6 +32,12 @@ W0EEEBot {self.version} :)""")
     async def on_ready(self):
         self.uls = UlsClient(await pg.create_pool(self.uls_db_url))
         #self.botpool = await pg.create_pool(self.bot_db_url)
+        
+        if self.listening_to is not None:
+            activity = discord.Activity(name=self.listening_to)
+            activity.type = discord.ActivityType.listening
+            
+            await self.change_presence(activity=activity)
         
         logger.info(f'Logged on as {self.user}')
     
