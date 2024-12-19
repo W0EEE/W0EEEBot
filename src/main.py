9a87fb0b02e78565cc904cc8f1547e0603c2f206
Eric Johnson <micromashor@gmail.com>
2024-12-19 17:30:49 -0600

    get version info from .version file

--- src/main.py
+++ src/main.py
@@ -10,6 +10,9 @@ from w0eeebot import W0eeeBot
 logger = logging.getLogger('main')
 
 def main() -> int | None:
+    with open("w0eeebot.version", "r") as version_fp:
+        version = version_fp.readline().strip()
+
     required_envvars = {
         'DISCORD_BOT_TOKEN': 'Discord bot token',
         'BOT_DB_URL': 'Postgres URL to access bot database',
@@ -22,7 +25,7 @@ def main() -> int | None:
             logger.error(f"Must provide {description}.")
             return 1
 
-    bot = W0eeeBot(os.environ['BOT_DB_URL'], os.environ['ULS_DB_URL'])
+    bot = W0eeeBot(os.environ['BOT_DB_URL'], os.environ['ULS_DB_URL'], version)
     bot.run(os.environ['DISCORD_BOT_TOKEN'], root_logger=True)
     
 if __name__ == '__main__':
--- src/w0eeebot.py
+++ src/w0eeebot.py
@@ -5,16 +5,16 @@ from commands import commands
 from db.uls import UlsClient
 import asyncpg as pg
 
-W0EEEBOT_VERSION = '3.0.0'
-
 logger = logging.getLogger("W0EEEBot")
 
 class W0eeeBot(discord.Client):
-    def __init__(self, bot_db_url: str, uls_db_url: str):        
+    version: str
+    def __init__(self, bot_db_url: str, uls_db_url: str, version: str):        
         super().__init__(intents=discord.Intents.default())
         
         self.bot_db_url = bot_db_url
         self.uls_db_url = uls_db_url
+        self.version = version
         
         root = app_commands.CommandTree(self)
         self.tree = root
@@ -23,7 +23,7 @@ class W0eeeBot(discord.Client):
         async def ping(interaction: discord.Interaction):
             """hey w0eeebot... you alive?"""
             await interaction.response.send_message(f"""Pong!
-W0EEEBot {self.version()} :)""")
+W0EEEBot {self.version} :)""")
         
         for cmd in commands.values():
             root.add_command(cmd.tree())
@@ -37,6 +37,3 @@ W0EEEBot {self.version()} :)""")
     async def setup_hook(self):
         await self.tree.sync()
         logger.info('Synced application commands')
-        
-    def version(self = None) -> str:
-        return W0EEEBOT_VERSION
